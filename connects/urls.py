b80044faf0fe5f8b2b5bff1aa14b19043a9b1f6e
Devondint <duonganhkhoi19082008@gmail.com>
2025-10-24 11:01:26 +0700

    change file logo and add subpage

--- connects/urls.py
+++ connects/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('tools/', views.tools, name='tools'),
+    path('library/', views.library, name='library'),
+    path('documents/', views.documents, name='documents'),
 ]
--- connects/views.py
+++ connects/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def tools(request):
+    return render(request, 'tools.html')
+
+def library(request):
+    return render(request, 'library.html')
+
+def documents(request):
+    return render(request, 'documents.html')
